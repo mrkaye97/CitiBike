0746487beb5c966013e9bfc41a7068b3e55ba075
Matt Kaye <mrkaye97@gmail.com>
2020-06-05 10:16:05 -0400

    Add some basic data import and plotting

--- R/citi-bike.R
+++ R/citi-bike.R
@@ -4,30 +4,44 @@ library(tidyverse)
 library(lubridate)
 library(ggmap)
 library(data.table)
+library(viridis)
 
 readfiles <- function(x) {
   temp <- tempfile() 
   download.file(paste('https://s3.amazonaws.com/tripdata/', x, '-citibike-tripdata.csv.zip', sep = ""), temp)
-  data <- temp %>%
-    unzip() %>%
-    fread()
+  data <- fread(cmd = paste('unzip -p', temp, sep = " "))
+  
+  do.call(file.remove, list(list.files(path = getwd(), pattern = '*.csv')))
+  
+  return(data)
 }
 
-d <- c(202001:202004) %>%
+raw <- c(202001:202004) %>%
   map(readfiles) %>%
-  rbindlist() %>%
+  rbindlist() 
+
+df <- raw %>%
   rename(sslat = 'start station latitude',
          sslon = 'start station longitude',
          eslat = 'end station latitude',
          eslon = 'end station longitude',
          ssid = 'start station id',
          esid = 'end station id') %>%
-  group_by(ssid) %>%
-  mutate(numstarts = n()) %>%
-  ungroup()
+  group_by(ssid, sslat, sslon) %>%
+  summarize(numstarts = n()) %>%
+  ungroup() %>%
+  arrange(desc(numstarts)) %>%
+  slice(1:50)
+  
 
 manhattan_map <- get_map(location = 'new york, new york', maptype = "watercolor", zoom = 12)
 
 ggmap(manhattan_map)
 
-qmplot(sslon, sslat, data = temp, maptype = "toner-background", size = propstarts, alpha = .01) 
+qmplot(sslon, sslat, data = df, maptype = "toner-background", color = numstarts, size = numstarts)+
+  scale_color_viridis()+
+  scale_alpha_continuous()
+  
+  
+  
+  
